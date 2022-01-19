f34caeed803d9850760579caf57cd0227d27f448
moisesrj97 <moisesrj97@gmail.com>
2022-01-19 11:59:49 +0100

    add weatherAPI service interfaces

---
+++ src/interfaces/weatherAPI.interfaces.ts
@@ -0,0 +1,60 @@
+export interface WeatherI {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface NextHourPredictionI {
+  dt: number;
+  temp: number;
+  feelsLike: number;
+  humidity: number;
+  weather: WeatherI;
+}
+
+export interface NextDayPredictionI {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feelsLike: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  humidity: number;
+  weather: WeatherI;
+}
+
+export interface IWeatherAPI {
+  location: string;
+  country: string;
+  timezone: number;
+  petitionTimestamp: number;
+  currentWeather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  };
+  currentTempAndHumidity: {
+    temp: string;
+    feelsLike: string;
+    tempMin: string;
+    tempMax: string;
+    humidity: string;
+  };
+  currentSunrise: number;
+  currentSunset: number;
+  nextHoursPrediction: NextHourPredictionI[];
+  nextWeekPrediction: NextDayPredictionI[];
+}
