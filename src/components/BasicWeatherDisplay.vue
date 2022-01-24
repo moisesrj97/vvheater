223813da47184521fe6df0957ed072bb9ce4d09c
moisesrj97 <moisesrj97@gmail.com>
2022-01-24 09:26:48 +0100

    rename helloWorld component

---
+++ src/components/BasicWeatherDisplay.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ weatherData }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import getWeather from '@/services/weatherAPIService';
+
+export default defineComponent({
+  name: 'BasicWeatherDisplay',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      weatherData: {},
+    };
+  },
+  async beforeMount() {
+    this.weatherData = await getWeather('Madrid');
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <BasicWeatherDisplay msg="Welcome to VVeather" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import BasicWeatherDisplay from '@/components/BasicWeatherDisplay.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    BasicWeatherDisplay,
   },
 });
 </script>
