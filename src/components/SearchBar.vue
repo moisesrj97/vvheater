3f20686bfdf1c393153a21ea818dd64e0ef393a7
moisesrj97 <moisesrj97@gmail.com>
2022-01-24 09:36:57 +0100

    create searchbar component and import it in home

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    <input v-bind="inputValue" placeholder="Enter some location..." />
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchBar',
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <SearchBar />
     <BasicWeatherDisplay msg="Welcome to VVeather" />
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BasicWeatherDisplay from '@/components/BasicWeatherDisplay.vue'; // @ is an alias to /src
+import SearchBar from '@/components/SearchBar.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     BasicWeatherDisplay,
+    SearchBar,
   },
 });
 </script>
