6c6e5d88db9843d699f69482ca8984bb3b06547e
moisesrj97 <moisesrj97@gmail.com>
2022-01-25 12:48:54 +0100

    change home basic component name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <SearchBar v-on:emitInput="setLocation" />
-    <BasicWeatherDisplay :location-name="locationName" />
+    <HomeDisplay :location-name="locationName" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BasicWeatherDisplay from '@/components/BasicWeatherDisplay.vue'; // @ is an alias to /src
+import HomeDisplay from '@/components/HomeDisplay.vue'; // @ is an alias to /src
 import SearchBar from '@/components/SearchBar.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    BasicWeatherDisplay,
+    HomeDisplay,
     SearchBar,
   },
   data() {
