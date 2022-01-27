3d3fb3799543ee7b0fa18f8658ab7d01e5f8b938
moisesrj97 <moisesrj97@gmail.com>
2022-01-27 16:26:50 +0100

    add chart component

---
+++ src/components/WeatherGraphic.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <apexchart
+      width="1000"
+      type="line"
+      :options="options"
+      :series="series"
+      @markerClick="click"
+    ></apexchart>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'WheatherGraphic',
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example',
+          toolbar: {
+            show: false,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+        xaxis: {
+          categories: this.chartData.nextHours,
+        },
+        stroke: {
+          curve: 'smooth',
+        },
+        marks: {
+          size: 0,
+        },
+        tooltip: {
+          enabled: true,
+        },
+      },
+      series: [
+        {
+          name: 'temperature',
+          data: this.chartData.nextTemps,
+        },
+      ],
+    };
+  },
+  methods: {
+    click: (event, chartContext, { seriesIndex, dataPointIndex, config }) => {
+      console.log({ seriesIndex, dataPointIndex, config });
+    },
+  },
+});
+</script>
