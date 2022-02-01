ce761563ef86dadecfa0d79c28a991872b7d6f94
moisesrj97 <moisesrj97@gmail.com>
2022-02-01 15:56:13 +0100

    add display of next week day information

--- src/components/NextWeekDisplay.vue
+++ src/components/NextWeekDisplay.vue
@@ -6,10 +6,28 @@
         {{ day.weekDay }}
       </li>
     </ul>
+    <div>
+      <div v-for="(day, index) in nextWeekPredictionDisplay" v-bind:key="index">
+        <h3>{{ day.weekDay }} {{ capitalizedWeather }}</h3>
+        <img
+          :src="`${`http://openweathermap.org/img/wn/${day.weather.icon}@2x.png`}`"
+          :alt="day.weather.description.concat(' icon')"
+        />
+        <p>{{ day.temp.max }}ºC / {{ day.temp.min }}ºC</p>
+        <p>Humidity:{{ day.humidity }}%</p>
+        <p>Sunrise: {{ generateTime(day.sunrise) }}</p>
+        <p>Sunset: {{ generateTime(day.sunset) }}</p>
+        <p>Morning: {{ day.temp.morn }}ºC({{ day.feelsLike.morn }})</p>
+        <p>Day: {{ day.temp.day }}ºC({{ day.feelsLike.day }})</p>
+        <p>Evening: {{ day.temp.eve }}ºC({{ day.feelsLike.eve }})</p>
+        <p>Night: {{ day.temp.night }}ºC({{ day.feelsLike.night }})</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { NextDayPredictionI } from '@/interfaces/weatherAPI.interfaces';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -17,19 +35,28 @@ export default defineComponent({
   props: {
     nextWeekPrediction: {
       required: true,
-      type: Array,
+      type: Array as () => NextDayPredictionI[],
     },
   },
   computed: {
     nextWeekPredictionDisplay() {
       return this.nextWeekPrediction
-        .map((day) => ({
+        .map((day: NextDayPredictionI) => ({
           ...day,
           weekDay: `${new Date(new Date(0).setSeconds(day.dt)).getUTCDate()}/${
             new Date(new Date(0).setSeconds(day.dt)).getUTCMonth() + 1
           }`,
         }))
         .filter((e, i) => i > 0);
+    },
+  },
+  methods: {
+    generateTime(time: number) {
+      return new Date(new Date(0).setUTCSeconds(time))
+        .toLocaleTimeString()
+        .split(':')
+        .splice(0, 2)
+        .join(':');
     },
   },
 });
