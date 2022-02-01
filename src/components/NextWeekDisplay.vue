47a1ca1ceb6ffcc307b306dca32cb14d72404c8b
moisesrj97 <moisesrj97@gmail.com>
2022-02-01 15:59:00 +0100

    add toggle between days when clicking day indicator

--- src/components/NextWeekDisplay.vue
+++ src/components/NextWeekDisplay.vue
@@ -2,25 +2,61 @@
   <h2>7-day prediction</h2>
   <div>
     <ul>
-      <li v-for="(day, index) in nextWeekPredictionDisplay" v-bind:key="index">
+      <li
+        v-for="(day, index) in nextWeekPredictionDisplay"
+        v-bind:key="index"
+        @click="dayMarker = index"
+      >
         {{ day.weekDay }}
       </li>
     </ul>
     <div>
-      <div v-for="(day, index) in nextWeekPredictionDisplay" v-bind:key="index">
-        <h3>{{ day.weekDay }} {{ capitalizedWeather }}</h3>
+      <div>
+        <h3>
+          {{ nextWeekPredictionDisplay[dayMarker].weekDay }}
+          {{ capitalizedWeather }}
+        </h3>
         <img
-          :src="`${`http://openweathermap.org/img/wn/${day.weather.icon}@2x.png`}`"
-          :alt="day.weather.description.concat(' icon')"
+          :src="`${`http://openweathermap.org/img/wn/${nextWeekPredictionDisplay[dayMarker].weather.icon}@2x.png`}`"
+          :alt="
+            nextWeekPredictionDisplay[dayMarker].weather.description.concat(
+              ' icon'
+            )
+          "
         />
-        <p>{{ day.temp.max }}ºC / {{ day.temp.min }}ºC</p>
-        <p>Humidity:{{ day.humidity }}%</p>
-        <p>Sunrise: {{ generateTime(day.sunrise) }}</p>
-        <p>Sunset: {{ generateTime(day.sunset) }}</p>
-        <p>Morning: {{ day.temp.morn }}ºC({{ day.feelsLike.morn }})</p>
-        <p>Day: {{ day.temp.day }}ºC({{ day.feelsLike.day }})</p>
-        <p>Evening: {{ day.temp.eve }}ºC({{ day.feelsLike.eve }})</p>
-        <p>Night: {{ day.temp.night }}ºC({{ day.feelsLike.night }})</p>
+        <p>
+          {{ nextWeekPredictionDisplay[dayMarker].temp.max }}ºC /
+          {{ nextWeekPredictionDisplay[dayMarker].temp.min }}ºC
+        </p>
+        <p>Humidity:{{ nextWeekPredictionDisplay[dayMarker].humidity }}%</p>
+        <p>
+          Sunrise:
+          {{ generateTime(nextWeekPredictionDisplay[dayMarker].sunrise) }}
+        </p>
+        <p>
+          Sunset:
+          {{ generateTime(nextWeekPredictionDisplay[dayMarker].sunset) }}
+        </p>
+        <p>
+          Morning: {{ nextWeekPredictionDisplay[dayMarker].temp.morn }}ºC({{
+            nextWeekPredictionDisplay[dayMarker].feelsLike.morn
+          }})
+        </p>
+        <p>
+          Day: {{ nextWeekPredictionDisplay[dayMarker].temp.day }}ºC({{
+            nextWeekPredictionDisplay[dayMarker].feelsLike.day
+          }})
+        </p>
+        <p>
+          Evening: {{ nextWeekPredictionDisplay[dayMarker].temp.eve }}ºC({{
+            nextWeekPredictionDisplay[dayMarker].feelsLike.eve
+          }})
+        </p>
+        <p>
+          Night: {{ nextWeekPredictionDisplay[dayMarker].temp.night }}ºC({{
+            nextWeekPredictionDisplay[dayMarker].feelsLike.night
+          }})
+        </p>
       </div>
     </div>
   </div>
@@ -37,6 +73,11 @@ export default defineComponent({
       required: true,
       type: Array as () => NextDayPredictionI[],
     },
+  },
+  data(): { dayMarker: number } {
+    return {
+      dayMarker: 0,
+    };
   },
   computed: {
     nextWeekPredictionDisplay() {
