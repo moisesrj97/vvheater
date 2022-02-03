8902f78973d352b7053932c8ace90b199d9c8799
moisesrj97 <moisesrj97@gmail.com>
2022-02-03 12:03:31 +0100

    add styles to searchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,13 @@
 <template>
-  <form @submit.prevent="emitLocation">
-    <input v-model="inputValue" placeholder="Enter some location..." />
-    <button type="submit">Search</button>
+  <form @submit.prevent="emitLocation" class="location-form">
+    <input
+      v-model="inputValue"
+      placeholder="Enter some location..."
+      class="location-form__input"
+    />
+    <button type="submit" class="location-form__button">
+      <img src="@/assets/search-icon.png" alt="search button" />
+    </button>
   </form>
 </template>
 <script lang="ts">
@@ -21,3 +27,35 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="scss">
+.location-form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 95%;
+  &__input {
+    width: 100%;
+    padding: 0.5rem;
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 1.1rem;
+    font-weight: 400;
+    margin: 0;
+    color: white;
+    background-color: transparent;
+    border: 1px solid white;
+    border-radius: 15px;
+    outline: none;
+  }
+  &__button {
+    background-color: transparent;
+    color: transparent;
+    box-shadow: none;
+    border: none;
+    & img {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+  }
+}
+</style>
